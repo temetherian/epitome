b27429a14b15320e96b0373c6fd0f6602f0569de
Adam Cabrera <temetherian@gmail.com>
2017-10-05 16:13:43 -0700

    Finish basic support for additional tournaments

--- epitome.py
+++ epitome.py
@@ -3,14 +3,25 @@
 import cloudstorage as gcs
 import jinja2
 import os
+import urllib
 import webapp2
 
 from google.appengine.api import app_identity
+from google.appengine.ext import ndb
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class Tournament(ndb.Model):
+  title = ndb.StringProperty()
+  codeword = ndb.StringProperty()
+
+
+################
+# Web Handlers #
+################
 
 class FrontPage(webapp2.RequestHandler):
   def get(self):
@@ -19,67 +30,96 @@ class FrontPage(webapp2.RequestHandler):
 
 class NewTournamentPage(webapp2.RequestHandler):
   def get(self):
-    bucket_name = os.environ.get('BUCKET_NAME',
-                                 app_identity.get_default_gcs_bucket_name())
-    files = gcs.listbucket('/' + bucket_name)
-    existing_names = ",".join("'%s'" % f.filename.split('/')[2] for f in files)
-    template_values = {'existing_names': existing_names}
+    title = self.request.get('title', '')
+
+    tourneys = Tournament.query()
+    existing_names = ",".join("'%s'" % t.key.id() for t in tourneys)
+    template_values = {'existing_names': existing_names,
+                       'title': title}
     template = JINJA_ENVIRONMENT.get_template('templates/new.html')
     self.response.write(template.render(template_values))
 
 class CreateTournamentHandler(webapp2.RequestHandler):
   def post(self):
-    tourney_name = self.request.get('tname').lower()
+    shortname = self.request.get('shortname').lower()
+    title = self.request.get('title')
     codeword = self.request.get('codeword')
-    # then store these somewhere
+
+    if Tournament.get_by_id(shortname):
+      params = {'title': title}
+      self.redirect('/_to/new?' + urllib.urlencode(params))
+    else:
+      new_tournament = Tournament(id=shortname, title=title, codeword=codeword)
+      new_tournament.put()
+      self.redirect('/%s/manage' % shortname)
 
 class ServeTomeHandler(webapp2.RequestHandler):
-  def get(self, tourney_name):
-    tourney_name = tourney_name.lower()
+  def get(self, shortname):
+    shortname = shortname.lower()
     bucket_name = os.environ.get('BUCKET_NAME',
                                  app_identity.get_default_gcs_bucket_name())
-    filename = '/%s/%s/export.js' % (bucket_name, tourney_name)
+    filename = '/%s/%s/export.js' % (bucket_name, shortname)
     export_data = gcs.open(filename)
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.write(export_data.read())
     export_data.close()
 
 class ManageTournamentPage(webapp2.RequestHandler):
-  def get(self, tourney_name):
-    template_values = {'tourney_name': tourney_name}
+  def get(self, shortname):
+    template_values = {'shortname': shortname}
+    tourney = Tournament.get_by_id(shortname)
+    if not tourney:
+      self.response.set_status(404)
+      self.response.write('Tournament not found.')
+      return
+
     template = JINJA_ENVIRONMENT.get_template('templates/manage.html')
     self.response.write(template.render(template_values))
 
 class UpdateHandler(webapp2.RequestHandler):
-  def post(self, tourney_name):
-    tourney_name = tourney_name.lower()
+  def post(self, shortname):
+    shortname = shortname.lower()
+    tourney = Tournament.get_by_id(shortname)
+    if not tourney:
+      self.response.set_status(404)
+      self.response.write('Tournament not found.')
+      return
+
+    codeword = self.request.get('codeword')
+    if codeword != tourney.codeword:
+      self.response.set_status(403)
+      self.response.write("You didn't say the magic word.")
+      return
+
     bucket_name = os.environ.get('BUCKET_NAME',
                                  app_identity.get_default_gcs_bucket_name())
-    filename = '/%s/%s/export.js' % (bucket_name, tourney_name)
-    codeword = self.request.get('codeword')
-    # check that codeword is correct
+    filename = '/%s/%s/export.js' % (bucket_name, shortname)
     data = self.request.get('export')
     with gcs.open(filename, 'w', content_type='text/plain') as export_data:
-      export_data.write(data)
-    template_values = {'tourney_name': tourney_name} # and success information
-    template = JINJA_ENVIRONMENT.get_template('templates/manage.html')
-    self.response.write(template.render(template_values))
+      export_data.write(data.encode('utf-8'))
+
+    self.redirect('/%s/manage' % shortname)
 
 class TournamentPage(webapp2.RequestHandler):
-  def get(self, tourney_name):
-    tourney_name = tourney_name.lower()
-    template_values = {'tourney_name': tourney_name}
+  def get(self, shortname):
+    shortname = shortname.lower()
+    tourney = Tournament.get_by_id(shortname)
+    if not tourney:
+      self.response.set_status(404)
+      self.response.write('Tournament not found.')
+      return
+    template_values = {'shortname': shortname, 'title': tourney.title}
     template = JINJA_ENVIRONMENT.get_template('templates/tourney.html')
     self.response.write(template.render(template_values))
 
 ROUTES = [
   ('/', FrontPage),
-  ('/new', NewTournamentPage),
-  ('/create', CreateTournamentHandler),
+  ('/_to/new', NewTournamentPage),
+  ('/_to/create', CreateTournamentHandler),
   ('/([A-Za-z0-9\-]+)/tome.json', ServeTomeHandler),
-  ('/([A-Za-z0-9\-]+)/manage/', ManageTournamentPage),
+  ('/([A-Za-z0-9\-]+)/manage', ManageTournamentPage),
   ('/([A-Za-z0-9\-]+)/update', UpdateHandler),
-  ('/([A-Za-z0-9\-]+)/', TournamentPage),
+  ('/([A-Za-z0-9\-]+)[/]?', TournamentPage),
 ]
 
 app = webapp2.WSGIApplication(routes=ROUTES)
