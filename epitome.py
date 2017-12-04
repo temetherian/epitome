f5635ce477e84c011376b84b4fef1b979f6b0717
Adam Cabrera <temetherian@gmail.com>
2017-12-04 14:59:14 -0800

    Add form endpoints that return JSON for a basic API

--- epitome.py
+++ epitome.py
@@ -2,7 +2,9 @@
 
 import cloudstorage as gcs
 import jinja2
+import json
 import os
+import re
 import urllib
 import webapp2
 
@@ -19,6 +21,94 @@ class Tournament(ndb.Model):
   codeword = ndb.StringProperty()
   app = ndb.StringProperty(default='tome')
 
+################
+# API Handlers #
+################
+
+class ApiUsedNames(webapp2.RequestHandler):
+  def get(self):
+    """Return a JSON list of all shortnames already in use."""
+    self.response.headers['Content-Type'] = 'application/json'
+    r = {}
+    tourneys = Tournament.query()
+    existing_names = [k.id() for k in tourneys.iter(keys_only=True)]
+    r['names'] = existing_names
+    self.response.write(json.dumps(r))
+
+class ApiCreate(webapp2.RequestHandler):
+  def post(self):
+    """Create a new tournament in epiTOME.
+
+    The request should be application/x-www-form-urlencoded with:
+      shortname: the tournament to be updated
+      codeword: the secret codeword for that tournament
+      app: ('nrtm' or 'tome') the tournament app being used
+           optional -- when omitted, will act like 'tome'
+
+    The response is JSON containing:
+      success: (bool)
+      msg: (str) An error message (on failure) or the shortname (on success)
+    """
+    shortname = self.request.get('shortname').lower()
+    title = self.request.get('title')
+    codeword = self.request.get('codeword')
+    app = self.request.get('app')
+
+    self.response.headers['Content-Type'] = 'application/json'
+    r = {}
+    if not re.match('^[a-z0-9\-]{3,}$', shortname):
+      self.response.set_status(400)
+      r['success'] = False
+      r['msg'] = 'Shortnames must be at least 3 characters and use only a-z, 0-9, and hyphens.'
+    elif Tournament.get_by_id(shortname):
+      self.response.set_status(409)
+      r['success'] = False
+      r['msg'] = 'That shortname is already in use.'
+    else:
+      new_tournament = Tournament(id=shortname, title=title, codeword=codeword, app=app)
+      new_tournament.put()
+      r['success'] = True
+      r['msg'] = shortname
+
+    self.response.write(json.dumps(r))
+
+class ApiUpdate(webapp2.RequestHandler):
+  def post(self):
+    """Update a tournament's export data in the blobstore.
+
+    The request should be application/x-www-form-urlencoded with:
+      shortname: the tournament to be updated
+      codeword: the secret codeword for that tournament
+      export: the exported JSON
+
+    The response is JSON containing:
+      success: (bool)
+      msg: (optional str) an explanation of an unsuccessful update
+    """
+    shortname = self.request.get('shortname').lower()
+    codeword = self.request.get('codeword')
+
+    self.response.headers['Content-Type'] = 'application/json'
+    r = {}
+    tourney = Tournament.get_by_id(shortname)
+    if not tourney:
+      self.response.set_status(404)
+      r['success'] = False
+      r['msg'] = 'unknown tournament'
+    elif codeword != tourney.codeword:
+      self.response.set_status(403)
+      r['success'] = False
+      r['msg'] = 'incorrect codeword'
+    else:
+      bucket_name = os.environ.get('BUCKET_NAME',
+                                   app_identity.get_default_gcs_bucket_name())
+      filename = '/%s/%s/export.js' % (bucket_name, shortname)
+      data = self.request.get('export')
+      with gcs.open(filename, 'w', content_type='text/plain') as export_data:
+        export_data.write(data.encode('utf-8'))
+      r['success'] = True
+
+    self.response.write(json.dumps(r))
 
 ################
 # Web Handlers #
@@ -34,7 +124,7 @@ class NewTournamentPage(webapp2.RequestHandler):
     title = self.request.get('title', '')
 
     tourneys = Tournament.query()
-    existing_names = ",".join("'%s'" % t.key.id() for t in tourneys)
+    existing_names = ",".join("'%s'" % k.id() for k in tourneys.iter(keys_only=True))
     template_values = {'existing_names': existing_names,
                        'title': title}
     template = JINJA_ENVIRONMENT.get_template('templates/new.html')
@@ -118,6 +208,9 @@ class TournamentPage(webapp2.RequestHandler):
 
 ROUTES = [
   ('/', FrontPage),
+  ('/_api/usednames', ApiUsedNames),
+  ('/_api/create', ApiCreate),
+  ('/_api/update', ApiUpdate),
   ('/_to/new', NewTournamentPage),
   ('/_to/create', CreateTournamentHandler),
   ('/([A-Za-z0-9\-]+)/tome.json', ServeJsonHandler),
